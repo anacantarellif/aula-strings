ec21971b877afe71b889ba9be48000be130cab8b
cantarelliana <accffers@gmail.com>
2023-05-02 21:28:42 -0300

    exercícios

---
+++ main.js
@@ -0,0 +1,79 @@
+//exercícios de interpretação de código
+
+/*
+let array
+console.log('a. ', array)  //a. undefined
+
+array = null
+console.log('b. ', array) //b. null
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)// c. 11
+
+let i = 0
+console.log('d. ', array[i]) //d. 0
+
+array[i+1] = 19
+console.log('e. ', array) //e. array(11)
+
+const valor = array[i+6]
+console.log('f. ', valor) //f. 9
+*/
+/*
+const frase = prompt("Digite uma frase")
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length) //SUBI NUM ÔNIBUS EM MIRROCOS 27
+*/
+/*
+emailDoUsuario = prompt("digite seu email")
+nomeDoUsuario = prompt("digite seu nome")
+
+
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}`)
+*/
+/*
+array = ["pizza", "sushi", "poke", "massa", "strogonoff"]
+console.log(array)
+console.log(`Essas são as minhas comidas preferidas: \n ${array[0]} \n ${array[1]} \n ${array[2]} \n ${array[3]} \n ${array[4]}`)
+const comidaNova = prompt("Qual sua comida preferida?")
+array[1] = comidaNova
+console.log(array)
+*/
+/*
+let listaDeTarefas = []
+
+listaDeTarefas[0] = prompt("digite uma tarefa")
+listaDeTarefas[1] = prompt("digite uma tarefa")
+listaDeTarefas[2] = prompt("digite uma tarefa")
+
+listaDetarefas.push(tarefa1, tarefa2, tarefa3)
+
+console.log(listaDeTarefas)
+
+let indice = Number(prompt("Qual a tarefa que você já realizou?1, 2 ou 3?"))
+
+listaDeTarefas.splice(indice - 1,1)
+
+console.log(listaDeTarefas);
+*/
+/*
+const frase = prompt("Digite uma frase")
+array = frase.split(' ')
+
+console.log(array)
+*/
+/*
+frutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+
+let indice = frutas.findIndex(achar);
+
+function achar(frutas) {
+    return frutas === "Abacaxi"
+}
+
+console.log(indice, frutas.length)
+*/
+
+
+
+
+
